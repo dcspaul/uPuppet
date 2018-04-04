57b1b497035035e4006ffb45acc496159eb395f3
Ivan Georgiev <s1410984@sms.ed.ac.uk>
2018-04-04 20:34:05 +0100

    Minor Patch To Make Collector Test More Rigorous

--- Test/Extra_Features/Collectors/collector1.pp
+++ Test/Extra_Features/Collectors/collector1.pp
@@ -1,33 +1,29 @@
+#Extra_Features/Collectors/collector1.pp 
 #@UNSUPPORTED
 # Nodes include defined resources after all other statements, but before resource collectors
 # Define after collector
 
 define d (
-  $backupArg = false,
-  $pathArg = '/default',
-  $modeArg = 123
+  $backupArg = false 
   ) {
   file {'from_define':
     backup => $backupArg,
-    source => $pathArg,
     path => $path,
-    mode => $modeArg
   }    
 }
 
 node default {
   $backup=true
-  File <| backup == true |> {
-    backup => false
+  File <| path == '/new' |> {
+    backup => true
   }
   
   # Here $path is undef, therefore it does not pass forward
   d { "defined_res":
     backupArg => $backup,
-    pathArg => $path
   }
 
-  $path = '/path'
+  $path = '/new'
 
   # At this point the defined resources are added with the prevously collected arguments
   # At this point the collector runs with the backup variable
